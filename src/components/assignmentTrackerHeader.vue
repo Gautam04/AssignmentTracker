fb31b556e1a465f83c82dfc7a1f59e2a873603e3
Gautam0401 <narayanan_gautam@yahoo.com>
2020-07-26 01:05:43 +0530

    Basic register page without backend created

---
+++ src/components/assignmentTrackerHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id='trackerHeader'>
+        <h1>Assignment Tracker</h1>
+    </div>
+</template>
+    
+<script>
+export default {
+    name:'AssignmentTrackerHeader'
+}
+</script>
+
+<style scoped>
+#trackerHeader{
+    background-color: purple;
+    color:whitesmoke;
+     width: 50%;
+    padding: 1%;
+    text-align: center;
+    margin:0 auto;
+   
+    
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <AssignmentTrackerHeader/>
+        <form>
+            <input id='Username' placeholder="Enter username" type=""/><br>
+            <input id = 'Email' placeholder="Email" type="email"/><br>
+            <input id = 'Phone' placeholder="Phone Number" type="number"/><br>
+            <input id = 'Password' placeholder="Password" type="password"/><br>
+            <input id = 'ConfPass' placeholder="Confirm Password" type="password"/><br>
+            <button>Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+import AssignmentTrackerHeader from '../components/assignmentTrackerHeader.vue'
+
+export default {
+    name:'Register',
+    components:{
+        AssignmentTrackerHeader
+    }
+}
+</script>
+
+<style scoped>
+    input{
+        padding: 1%;
+        width: 50%;
+        
+    }
+    
+    button{
+    background: purple;
+    color: white;
+    padding: 0.5% 0.5%;
+    width:inherit;
+    
+}
+
+button:hover{
+    background: whitesmoke;
+    color: purple;
+    border-color: purple;
+}
+</style>
